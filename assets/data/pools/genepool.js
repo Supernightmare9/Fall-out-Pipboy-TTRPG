731ec647a765d515d83fa9ea60a139b11dfed864
Supernightmare9 <dbriggssa@gmail.com>
2026-04-14 10:38:09 -0400

    Create genePool.js with mutated creature data
    Added a new gene pool with various mutated creatures, each having unique descriptions and starting perks.

---
+++ assets/data/pools/genepool.js
@@ -0,0 +1,67 @@
+window.genePool = [
+  {
+    name: "Radroach",
+    description: "Small, pesky irradiated roaches often found in damp, dark places.",
+    startingPerk: "Eat Up: Eating food no longer gives you radiation damage"
+  },
+  {
+    name: "Rooken",
+    description: "An irradiated bird that seems to be a mix of a rooster & chicken.",
+    startingPerk: "What Came First: You gain +2 to initiative"
+  },
+  {
+    name: "Barhim",
+    description: "A mutated cow that most traders use to transport goods.",
+    startingPerk: "Strong Back: Add +50 extra carry weight"
+  },
+  {
+    name: "Mirelurk",
+    description: "Armored crustaceans that evolved from crabs; highly defensive shell.",
+    startingPerk: "Hard Shell: +2 to AC"
+  },
+  {
+    name: "Yao Guai",
+    description: "Mutated black bears that are incredibly territorial and fierce.",
+    startingPerk: "Animal Instinct: Gain 1 action surge per long rest"
+  },
+  {
+    name: "Bloatfly",
+    description: "Flying insects that shoot larvae at their prey from a distance.",
+    startingPerk: "Larva Shot: Can shoot a poisonous larva dealing 1d6 damage and 1d4 poison damage to the target once per turn for 1d6 turns"
+  },
+  {
+    name: "Bloodbug",
+    description: "A large insect, with a needle-like nose for sucking the blood from its victims.",
+    startingPerk: "Sucked Dry: You gain 1d4 of health for every successful attack"
+  },
+  {
+    name: "Radspiders",
+    description: "An eight-legged arachnid that hides in the shadows.",
+    startingPerk: "One With The Shadows: Once a day you may use this perk to disappear into a darkened area"
+  },
+  {
+    name: "Radscorpions",
+    description: "Large insectoids with a massive stinger and two big meaty claws.",
+    startingPerk: "Sting: Gain a sting attack, you do 1d6 damage plus 1d4 poison damage for 1d4 turns"
+  },
+  {
+    name: "Mole Rats",
+    description: "Subterranean vermin that pop up to attack to catch prey off guard.",
+    startingPerk: "Dig Dug: You have the ability to traverse underground. This is at half movement but during this turn you cannot take damage unless from a specialized attack"
+  },
+  {
+    name: "Radghast",
+    description: 'A two-headed mutated "deer".',
+    startingPerk: "Oh Deer: Gain an additional +10 ft to movement speed"
+  },
+  {
+    name: "Ants",
+    description: "Large insectoids that have grown ten times in size and strength.",
+    startingPerk: "Two by Two: May summon two ant allies per day"
+  },
+  {
+    name: "Harpy",
+    description: "Birds of prey who have mutated over the years, stretched to abnormal length, with a vicious nature dangerous to settlers.",
+    startingPerk: "Banshee Scream: Once a day, use a scream with a reach of 20 ft in a cone radius to confuse your foe on an unsuccessful Endurance Saving Throw"
+  }
+];
