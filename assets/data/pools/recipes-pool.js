0fa0ccc828ed995ba831ac9a85053e271ce2cbca
Supernightmare9 <dbriggssa@gmail.com>
2026-04-15 20:41:07 -0400

    Add new recipes to recipes-pool.js

--- assets/data/pools/recipes-pool.js
+++ assets/data/pools/recipes-pool.js
@@ -16,5 +16,30 @@ export const recipesPool = [
   {
     name: "Wasteland Omelette",
     category: "Food",
-    ingredients: ["Ant Egg", "Bighorner Meat", "Pur](#)
-
+    ingredients: ["Ant Egg", "Bighorner Meat", "Purified Water"],
+    requiredSkill: "Survival",
+    effect: "Restores 15 HP"
+  },
+  {
+    name: "Weapon Repair Kit",
+    category: "Utility",
+    ingredients: ["Wrench", "Duct Tape", "Wonderglue", "Steel"],
+    requiredSkill: "Repair",
+    effect: "Restores any broken weapon"
+  },
+  {
+    name: "Homemade Jet",
+    category: "Chem",
+    ingredients: ["Plastic", "Fertilizer", "Acid"],
+    requiredSkill: "Science",
+    effect: "Temp +10 AP"
+  },
+  {
+    name: "Purified Water",
+    category: "Drink",
+    ingredients: ["Dirty Water", "Glass", "Charcoal"],
+    requiredSkill: "Survival",
+    effect: "Restores 10 HP, no Rads"
+  },
+  // ... Add other recipes similarly
+];
