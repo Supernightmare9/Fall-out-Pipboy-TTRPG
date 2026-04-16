1f869fa4fe0f1a8096e5d5c27bf4551c9ad611ad
Supernightmare9 <dbriggssa@gmail.com>
2026-04-15 20:40:14 -0400

    Add recipes pool with various items

--- assets/data/pools/recipes-pool.js
+++ assets/data/pools/recipes-pool.js
@@ -1,1 +1,20 @@
+export const recipesPool = [
+  {
+    name: "Improvised Stimpak",
+    category: "Aid",
+    ingredients: ["Blood Pack", "Antiseptic", "Steel"],
+    requiredSkill: "Medicine",
+    effect: "Restores 10 HP"
+  },
+  {
+    name: "Rad-X Alternative",
+    category: "Aid",
+    ingredients: ["Bloodleaf", "Agave Fruit", "Glass"],
+    requiredSkill: "Survival",
+    effect: "+15 Rad Healing"
+  },
+  {
+    name: "Wasteland Omelette",
+    category: "Food",
+    ingredients: ["Ant Egg", "Bighorner Meat", "Pur](#)
 
